db3527a367a1f5022c14ee7041c441ac3a25c7e2
DAFU <34666601+kikiriko200@users.noreply.github.com>
2018-06-03 20:12:35 +0900

    Add files via upload

---
+++ template/js/temp.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#head").load("/temp/header.php");
+    $("#footer").load("/temp/footer.php");
+    $("#cent").load("/temp/cent.php");
+    $("#nav").load("/temp/nav.php");
+  });
